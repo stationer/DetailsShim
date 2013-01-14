fefa37b5d182719d92d52793c0f43b52bc222ac5
Tyler Uebele <tyler.uebele@bettercarpeople.com>
2013-01-14 13:13:32 -0500

    details-shim initial commit

--- README.md
+++ README.md
@@ -2,3 +2,33 @@ details-shim
 ============
 
 Make HTML5 Details/Summary tags work in unsupportive browsers
+
+Usage
+=====
+
+Include details-shim.js anywhere in your page, it runs on load
+Include details-shim.css (or its contents) in your styles
+
+Support
+=======
+
+Tested successfully in FireFox Win/Mac/Lin
+Non-interfering in Chrome Win/Mac/Lin, which supports details/summary
+Non-interfering in Safari Mac, which supports details/summary
+Tested successfully in Internet Explorer 9
+Tested mostly successfully in Internet Explorer 8 with html5shiv
+ (IE doesn't like the unicode css :before content)
+
+
+Known Issues
+============
+
+FireFox changes <details open> to <details open="">
+Safari supports <details> natively, but treats `open=""` as `open="open"`
+
+
+License
+=======
+
+Released under MIT license, see included LICENSE.txt
+
---
+++ details-shim.js
@@ -0,0 +1,53 @@
+/* https://github.com/tyleruebele/details-shim */
+function details_shim(){
+	//Because <details> must include a <summary>, 
+	// collecting <summary> tags collects *valid* <details> tags
+	var a = document.getElementsByTagName('summary');
+	for (f in a) {
+		if (a[f].parentNode) {
+			var details = a[f].parentNode;
+			//only run in browsers that don't support <details> natively
+			if ('boolean' != typeof details.open) {
+			
+				//Set initial class according to `open` attribute
+				var s = details.outerHTML;
+				s = s.substring(0, s.indexOf('>'));
+				s = (-1 != s.indexOf('open') && -1 == s.indexOf('open=""')) 
+					? 'open' : 'closed';
+				details.setAttribute('data-open', s);
+				details.className += ' ' + s;
+
+				//Add onclick handler to toggle visibility class
+				a[f].onclick = function() {
+					//current state
+					var s = this.parentNode.getAttribute('data-open');
+					//new state
+					s = s == 'open' ? 'closed' : 'open';
+					this.parentNode.setAttribute('data-open', s);
+					//replace previous open/close class
+					this.parentNode.className = this.parentNode.className
+						.replace(/\bopen\b|\bclosed\b/g, ' ') + ' ' + s;
+				};
+				
+				//wrap text nodes in span to expose them to css
+				var sibs = details.childNodes;
+				for (ff in sibs) {
+					if (sibs[ff].nodeType == 3 
+						&& /[^\s]/.test(sibs[ff].data)
+					) {
+						var span = document.createElement('span');
+						var text = sibs[ff];
+						details.insertBefore(span, text);
+						details.removeChild(text);
+						span.appendChild(text);
+					}
+				}
+			}
+		}
+	}
+}
+
+window.addEventListener
+? window.addEventListener('load', details_shim, false)
+: window.attachEvent && window.attachEvent('onload', details_shim)
+;
