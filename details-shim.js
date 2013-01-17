d4cea7b64a42a509944bc5a79a9d8a49a7643be2
Tyler Uebele <tyler.uebele@bettercarpeople.com>
2013-01-17 15:13:35 -0500

    Correct Typo

--- details-shim.js
+++ details-shim.js
@@ -1,6 +1,6 @@
 /* https://github.com/tyleruebele/details-shim */
 function details_shim(){
-	//Because <details> must include a <summary>, 
+	//Because <details> must include a <summary>,
 	// collecting <summary> tags collects *valid* <details> tags
 	var a = document.getElementsByTagName('summary');
 	for (f in a) {
@@ -8,11 +8,11 @@ function details_shim(){
 			var details = a[f].parentNode;
 			//only run in browsers that don't support <details> natively
 			if ('boolean' != typeof details.open) {
-			
+
 				//Set initial class according to `open` attribute
 				var s = details.outerHTML;
 				s = s.substring(0, s.indexOf('>'));
-				s = (-1 != s.indexOf('open') && -1 == s.indexOf('open=""')) 
+				s = (-1 != s.indexOf('open') && -1 == s.indexOf('open=""'))
 					? 'open' : 'closed';
 				details.setAttribute('data-open', s);
 				details.className += ' ' + s;
@@ -28,11 +28,11 @@ function details_shim(){
 					this.parentNode.className = this.parentNode.className
 						.replace(/\bopen\b|\bclosed\b/g, ' ') + ' ' + s;
 				};
-				
+
 				//wrap text nodes in span to expose them to css
 				var sibs = details.childNodes;
 				for (ff in sibs) {
-					if (sibs[ff].nodeType == 3 
+					if (sibs[ff].nodeType == 3
 						&& /[^\s]/.test(sibs[ff].data)
 					) {
 						var span = document.createElement('span');
